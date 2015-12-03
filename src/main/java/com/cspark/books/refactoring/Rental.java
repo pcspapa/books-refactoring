7beffa798e829da2de2b943ece7349b40cd9fd03
pcspapa <pcspapa@gmail.com>
2015-12-03 14:49:24 +0900

    “initail”

---
+++ src/main/java/com/cspark/books/refactoring/Rental.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.cspark.books.refactoring;
+
+/**
+ * Created by cspark on 2015. 11. 26..
+ */
+public class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this._movie = movie;
+        this._daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+}
