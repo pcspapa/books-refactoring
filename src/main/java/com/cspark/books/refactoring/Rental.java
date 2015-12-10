d4696569fc7cabdab4e0732d077d1b2a3885ba58
pcspapa <pcspapa@gmail.com>
2015-12-10 11:20:09 +0900

    final

--- src/main/java/com/cspark/books/refactoring/Rental.java
+++ src/main/java/com/cspark/books/refactoring/Rental.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
 package com.cspark.books.refactoring;
 
 /**
@@ -22,10 +14,6 @@ public class Rental {
 
     public Movie getMovie() {
         return _movie;
-    }
-
-    public int getDaysRented() {
-        return _daysRented;
     }
 
     double getCharge() {
